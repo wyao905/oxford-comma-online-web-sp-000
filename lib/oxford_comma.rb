7b048ffb837dee7f643c4153cd161f184179d327
Warren Yao <warrenyca905@gmail.com>
2019-05-19 03:02:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length == 1
+    array.join
+  else
+    array[-1].prepend "and "
+    array.join(", ")
+  end
 end
